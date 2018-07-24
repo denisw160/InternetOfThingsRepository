ab3dbeb8b12981ca3b225c3905075d0c74524d85
Denis Wirries <denis@wirries.me>
2018-07-24 18:55:37 +0200

    more parameter

--- thinger-sensortag/run.sh
+++ thinger-sensortag/run.sh
@@ -3,4 +3,6 @@ mkdir -p build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DDAEMON=OFF ../
 make thinger
-./thinger
+./thinger C4:BE:84:70:F6:8B=sensorRed A0:E6:F8:AE:37:80=sensorYellow
+#./thinger A0:E6:F8:AE:37:80=sensorYellow
+#./thinger
