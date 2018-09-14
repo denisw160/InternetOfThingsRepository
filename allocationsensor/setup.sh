eb0bf725079283d7a0e86648d916663a58203d02
Denis Wirries <denis@wirries.me>
2018-09-14 16:22:16 +0200

    add more components

--- allocationsensor/setup.sh
+++ allocationsensor/setup.sh
@@ -3,4 +3,11 @@
 # This scripts installs necessary components
 #
 
+sudo apt update
+sudo apt upgrade
+sudo apt autoremove
+
+sudo apt-get install python-pip
+#sudo apt-get install python3-pip
+
 sudo pip install PyCRC
