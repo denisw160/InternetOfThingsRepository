4fddb7f0269a8c25e5e81f78cf410eca8a75be3c
Denis Wirries <denis@wirries.me>
2018-09-17 16:31:18 +0200

    fix

--- thinger-sensortag/test.sh
+++ thinger-sensortag/test.sh
@@ -5,4 +5,4 @@ echo Searching for bluetooth devices - press CRTL-C to stop
 sudo hcitool lescan
 
 # Run python script for testing
-sudo python src/sensortagcollector.py C4:BE:84:70:F6:8B=sensorRed A0:E6:F8:AE:37:80=sensorYellow
+sudo python src/sensortagcollector.py -d C4:BE:84:70:F6:8B=sensorRed A0:E6:F8:AE:37:80=sensorYellow
