931591add7d9ac45d5df7dbdf123c1c50fab6480
Denis Wirries <denis@wirries.me>
2018-09-17 17:18:20 +0200

    adding dependency

--- thinger-sensortag/setup.sh
+++ thinger-sensortag/setup.sh
@@ -8,6 +8,7 @@ sudo apt upgrade
 sudo apt autoremove
 
 sudo apt-get install -y cmake
+sudo apt-get install -y wiringpi
 sudo apt-get install -y libssl-dev
 sudo apt-get install -y libjsoncpp-dev
 sudo apt-get install -y bluez build-essential libglib2.0-dev libdbus-1-dev python-dev
