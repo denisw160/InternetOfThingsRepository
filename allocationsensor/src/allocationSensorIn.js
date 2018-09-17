98bd21f5df712fc3bbdda8f3049174cc20b51f53
Denis Wirries <denis@wirries.me>
2018-09-17 12:24:21 +0200

    Adding scripts

---
+++ allocationsensor/src/allocationSensorIn.js
@@ -0,0 +1,7 @@
+// INPUT 0 true DemoUser
+// buffer contains complete input telegram from target
+
+buffer.position(8); // jump to payload index
+
+var allocated = buffer.get(); // extract byte 1
+self.log("allocation from target = " + allocated);
---
+++ allocationsensor/src/allocationSensorOut.js
@@ -0,0 +1,18 @@
+// OUTPUT 0 true DemoUser
+// buffer contains complete output telegram from server
+
+buffer.position(8); // jump to playoad index
+
+// define byte array for payload
+var payload = Java.to([0], "byte[]");
+
+// Allocation state
+var allocated = t.getStatus("allocated");
+self.log("allocation state = " + allocated);
+
+h.setBit(payload, 0, allocated == true);
+
+// put payload into buffer
+buffer.put(payload);
+
+// checksum will be automatically added by system
