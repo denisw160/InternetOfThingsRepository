87a54153b93f01641e4e1ac455283170e205db54
Denis Wirries <denis@wirries.me>
2018-09-17 17:52:21 +0200

    format, more sensor values, preparation for reading, removing old structs

--- thinger-sensortag/src/main.cpp
+++ thinger-sensortag/src/main.cpp
@@ -26,6 +26,7 @@
 #include <fstream>
 #include <string>
 #include <thread>
+#include <map>
 
 #include <jsoncpp/json/json.h>
 #include <jsoncpp/json/reader.h>
@@ -36,23 +37,21 @@
 
 using namespace std;
 
-#define USER_ID 			"xxx"
-#define DEVICE_ID 			"xxx"
-#define DEVICE_CREDENTIAL 	"xxx"
+#define USER_ID             "izb"
+#define DEVICE_ID           "showcase"
+#define DEVICE_CREDENTIAL   "password"
 
-#define SENSORTAG_CMD 		"python ./../../sensortag/sensortagcollector.py -d "
+#define SENSORTAG_CMD       "python sensortagcollector.py -d "
 
-class Load
-{
-  public:
+class Load {
+public:
     float load1min;
     float load3min;
     float load5min;
 };
 
-class Memory
-{
-  public:
+class Memory {
+public:
     int total;
     int free;
     int available;
@@ -61,176 +60,144 @@ class Memory
     int swapFree;
 };
 
-class Sensor
-{
-  public:
-    float x;
-    float y;
-    float z;
-	// TODO add more data
+// Sample
+//  {"deviceuid":"a0:e6:f8:ae:37:80","devicename":"sensorYellow","accelerometer":[0.094482421875,0.034912109375,0.8818359375]}
+//  {"deviceuid":"a0:e6:f8:ae:37:80","devicename":"sensorYellow","lightmeter":67.56}
+//  {"deviceuid":"a0:e6:f8:ae:37:80","devicename":"sensorYellow","humidity":[27.2225952148,42.2302246094]}
+//  {"deviceuid":"a0:e6:f8:ae:37:80","devicename":"sensorYellow","barometer":[27.24,1012.23]}
+
+class Sensor {
+public:
+    float accelerometerX;
+    float accelerometerY;
+    float accelerometerZ;
+    float lightmeter;
+    float temperature;
+    float humidity;
+    float barometer;
 };
 
 map<string, Sensor> sensors;
 
-/// old
-
-class Accelerometer
-{
-  public:
-    float x;
-    float y;
-    float z;
-};
-
-Accelerometer sensorRedAccelerometer;
-Accelerometer sensorYellowAccelerometer;
-
-/// old end
-
-Load getLoad()
-{
-    Load load;
+Load getLoad() {
+    Load load{};
     int fileHandler;
     char fileBuffer[1024];
 
     fileHandler = open("/proc/loadavg", O_RDONLY);
-    if (fileHandler < 0)
-    {
+    if (fileHandler < 0) {
         return load;
     }
     read(fileHandler, fileBuffer, sizeof(fileBuffer) - 1);
-    stringstream ssin(fileBuffer);
-    if (ssin.good())
-    {
-        ssin >> load.load1min;
+    stringstream inStream(fileBuffer);
+    if (inStream.good()) {
+        inStream >> load.load1min;
     }
-    if (ssin.good())
-    {
-        ssin >> load.load3min;
+    if (inStream.good()) {
+        inStream >> load.load3min;
     }
-    if (ssin.good())
-    {
-        ssin >> load.load5min;
+    if (inStream.good()) {
+        inStream >> load.load5min;
     }
     close(fileHandler);
 
-    //cout << fileBuffer;
     return load;
 }
 
-Memory getMemory()
-{
-    Memory memory;
-    int fileHandler;
-    char fileBuffer[1024];
+Memory getMemory() {
+    Memory memory{};
 
     ifstream infile("/proc/meminfo");
     string line;
-    while (getline(infile, line))
-    {
-        stringstream ssin(line);
-        if (!line.compare(0, 8, "MemTotal"))
-        {
-            ssin.ignore(256, ' ');
-            ssin >> memory.total;
-        }
-        else if (!line.compare(0, 7, "MemFree"))
-        {
-            ssin.ignore(256, ' ');
-            ssin >> memory.free;
-        }
-        else if (!line.compare(0, 12, "MemAvailable"))
-        {
-            ssin.ignore(256, ' ');
-            ssin >> memory.available;
-        }
-        else if (!line.compare(0, 6, "Cached"))
-        {
-            ssin.ignore(256, ' ');
-            ssin >> memory.cached;
-        }
-        else if (!line.compare(0, 9, "SwapTotal"))
-        {
-            ssin.ignore(256, ' ');
-            ssin >> memory.swapTotal;
-        }
-        else if (!line.compare(0, 8, "SwapFree"))
-        {
-            ssin.ignore(256, ' ');
-            ssin >> memory.swapFree;
+    while (getline(infile, line)) {
+        stringstream inStream(line);
+        if (!line.compare(0, 8, "MemTotal")) {
+            inStream.ignore(256, ' ');
+            inStream >> memory.total;
+        } else if (!line.compare(0, 7, "MemFree")) {
+            inStream.ignore(256, ' ');
+            inStream >> memory.free;
+        } else if (!line.compare(0, 12, "MemAvailable")) {
+            inStream.ignore(256, ' ');
+            inStream >> memory.available;
+        } else if (!line.compare(0, 6, "Cached")) {
+            inStream.ignore(256, ' ');
+            inStream >> memory.cached;
+        } else if (!line.compare(0, 9, "SwapTotal")) {
+            inStream.ignore(256, ' ');
+            inStream >> memory.swapTotal;
+        } else if (!line.compare(0, 8, "SwapFree")) {
+            inStream.ignore(256, ' ');
+            inStream >> memory.swapFree;
         }
     }
 
     return memory;
 }
 
-void streamSensorTag(thinger_device thing)
-{
-    FILE *fp;
-    char json[1035];
-
-    /* Open the command for reading. */
-    fp = popen(SENSORTAG_CMD, "r");
-    if (fp == NULL)
-    {
-        printf("Failed to run command\n");
-        exit(1);
-    }
-
-    Json::Value root;
-    Json::Reader reader;
-
-    /* Read the output a line at a time - output it. */
-    while (fgets(json, sizeof(json) - 1, fp) != NULL)
-    {
-        //printf("%s", json);
-
-        bool parsingSuccessful = reader.parse(json, root); //parse process
-        if (!parsingSuccessful)
-        {
-            cout << "Failed to parse" << reader.getFormattedErrorMessages();
-            exit(2);
-        }
-
-        if (root.get("devicename", "none") == "sensorRed" && root.isMember("accelerometer"))
-        {
-            Json::Value acc = root["accelerometer"];
-            sensorRedAccelerometer.x = acc[0].asFloat();
-            sensorRedAccelerometer.y = acc[1].asFloat();
-            sensorRedAccelerometer.z = acc[2].asFloat();
-        }
-        else if (root.get("devicename", "none") == "sensorYellow" && root.isMember("accelerometer"))
-        {
-            Json::Value acc = root["accelerometer"];
-            sensorYellowAccelerometer.x = acc[0].asFloat();
-            sensorYellowAccelerometer.y = acc[1].asFloat();
-            sensorYellowAccelerometer.z = acc[2].asFloat();
-        }
-
-        // TODO Weitere Sensor-Daten (Licht, Temperatur)
-    }
-
-    /* close */
-    pclose(fp);
-    printf("SensorTag terminated\n");
+void streamSensorTag(thinger_device thing) {
+//    FILE *fp;
+//    char json[1035];
+//
+//    /* Open the command for reading. */
+//    fp = popen(SENSORTAG_CMD, "r");
+//    if (fp == NULL) {
+//        printf("Failed to run command\n");
+//        exit(1);
+//    }
+//
+//    Json::Value root;
+//    Json::Reader reader;
+//
+//    /* Read the output a line at a time - output it. */
+//    while (fgets(json, sizeof(json) - 1, fp) != NULL) {
+//        //printf("%s", json);
+//
+//        bool parsingSuccessful = reader.parse(json, root); //parse process
+//        if (!parsingSuccessful) {
+//            cout << "Failed to parse" << reader.getFormattedErrorMessages();
+//            exit(2);
+//        }
+//
+//        if (root.get("devicename", "none") == "sensorRed" && root.isMember("accelerometer")) {
+//            Json::Value acc = root["accelerometer"];
+//            sensorRedAccelerometer.x = acc[0].asFloat();
+//            sensorRedAccelerometer.y = acc[1].asFloat();
+//            sensorRedAccelerometer.z = acc[2].asFloat();
+//        } else if (root.get("devicename", "none") == "sensorYellow" && root.isMember("accelerometer")) {
+//            Json::Value acc = root["accelerometer"];
+//            sensorYellowAccelerometer.x = acc[0].asFloat();
+//            sensorYellowAccelerometer.y = acc[1].asFloat();
+//            sensorYellowAccelerometer.z = acc[2].asFloat();
+//        }
+//
+//        // TODO Weitere Sensor-Daten (Licht, Temperatur)
+//    }
+//
+//    /* close */
+//    pclose(fp);
+//    printf("SensorTag terminated\n");
 }
 
 // Arguments are C4:BE:84:70:F6:8B=sensorRed A0:E6:F8:AE:37:80=sensorYellow
-int main(int argc, char* argv[])
-{
-	printf("argc: %i\n", argc);
-	
-	for(int i = 0; i < argc; ++i) {
-  		printf("argv %i: %s\n", i, argv[i]);
-	}
-	
-	if (argc < 2) {
-		printf("No arguments found, please use C4:BE:84:70:F6:8B=sensorRed A0:E6:F8:AE:37:80=sensorYellow or something like that\n");
-		exit(-1);
-	}
-	
+int main(int argc, char *argv[]) {
+    printf("Starting Gateway for SensorTags\n");
+    printf(" Reading from %i sensors\n", argc - 1);
+
+//    // only for debugging
+//    for (int i = 0; i < argc; ++i) {
+//        printf("argv %i: %s\n", i, argv[i]);
+//    }
+
+    if (argc < 2) {
+        printf("No arguments found, please use C4:BE:84:70:F6:8B=sensorRed A0:E6:F8:AE:37:80=sensorYellow or something like that\n");
+        exit(-1);
+    }
+
+    // Register on Thinger.io server (iot.thinger.io)
     thinger_device thing(USER_ID, DEVICE_ID, DEVICE_CREDENTIAL);
 
+    // Add API for system information
     thing["load"] >> [](pson &out) {
         Load l = getLoad();
         out["load1min"] = l.load1min;
@@ -250,39 +217,27 @@ int main(int argc, char* argv[])
         out["swapFree"] = m.swapFree;
     };
 
-	// register sensors
-	for(int i = 1; i < argc; ++i) {
-  		printf("Adding sensor %i: %s\n", i, argv[i]);
-		
-		char* pch;
-		static char* sensor;
-  		pch = strtok (argv[i], "=");
-		pch = strtok (NULL, "=");
-		
-		sensor = pch;
-		printf("%s\n", sensor);
-		Sensor s;
-		sensors[sensor] = s;
-		
-		thing[sensor] >> [](pson &out) {
-        	out["x"] = sensors[sensor].x;
-        	out["y"] = 2;//sensors[sensor].y;
-        	out["z"] = 3;//sensors[sensor].z;
-    	};
-	}
-	
-	
-    thing["sensorRedAccelerometer"] >> [](pson &out) {
-        out["x"] = sensorRedAccelerometer.x;
-        out["y"] = sensorRedAccelerometer.y;
-        out["z"] = sensorRedAccelerometer.z;
-    };
+    // Adding API for sensors from command line
+    for (int i = 1; i < argc; ++i) {
+        printf(" Adding sensor %i: %s\n", i, argv[i]);
 
-    thing["sensorYellowAccelerometer"] >> [](pson &out) {
-        out["x"] = sensorYellowAccelerometer.x;
-        out["y"] = sensorYellowAccelerometer.y;
-        out["z"] = sensorYellowAccelerometer.z;
-    };
+        static char *sensorName;
+        strtok(argv[i], "=");
+        sensorName = strtok(nullptr, "=");
+
+        Sensor sensor{};
+        sensors[sensorName] = sensor;
+
+        thing[sensorName] >> [](pson &out) {
+            out["accelerometerX"] = sensors[sensorName].accelerometerX;
+            out["accelerometerY"] = sensors[sensorName].accelerometerY;
+            out["accelerometerZ"] = sensors[sensorName].accelerometerZ;
+            out["lightmeter"] = sensors[sensorName].lightmeter;
+            out["temperature"] = sensors[sensorName].temperature;
+            out["humidity"] = sensors[sensorName].humidity;
+            out["barometer"] = sensors[sensorName].barometer;
+        };
+    }
 
     thread t(streamSensorTag, thing);
     thing.start();
