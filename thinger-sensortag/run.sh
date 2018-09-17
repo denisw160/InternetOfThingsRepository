55869ad9b7fb6ca4a7fd044c0ef56e54cb27857b
Denis Wirries <denis@wirries.me>
2018-09-17 16:43:36 +0200

    adding sensors

--- thinger-sensortag/run.sh
+++ thinger-sensortag/run.sh
@@ -2,6 +2,6 @@
 ./build.sh
 cd build
 
-sudo ./thinger C4:BE:84:70:F6:8B=sensorRed A0:E6:F8:AE:37:80=sensorYellow
+sudo ./thinger C4:BE:84:70:F6:8B=sensorRed A0:E6:F8:AE:37:80=sensorYellow B0:91:22:F6:CF:87=sensorViolet 54:6C:0E:4D:CC:02=sensorBlue 54:6C:0E:4D:5E:80=sensorOrange B0:91:22:F6:E6:02=sensorGreen
 #sudo ./thinger A0:E6:F8:AE:37:80=sensorYellow
 #sudo ./thinger
--- thinger-sensortag/test.sh
+++ thinger-sensortag/test.sh
@@ -5,4 +5,4 @@ echo Searching for bluetooth devices - press CRTL-C to stop
 sudo hcitool lescan
 
 # Run python script for testing
-sudo python src/sensortagcollector.py -d C4:BE:84:70:F6:8B=sensorRed A0:E6:F8:AE:37:80=sensorYellow
+sudo python src/sensortagcollector.py -d C4:BE:84:70:F6:8B=sensorRed A0:E6:F8:AE:37:80=sensorYellow B0:91:22:F6:CF:87=sensorViolet 54:6C:0E:4D:CC:02=sensorBlue 54:6C:0E:4D:5E:80=sensorOrange B0:91:22:F6:E6:02=sensorGreen
