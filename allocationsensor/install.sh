1f686c4bac8001459f9ffdca1696279093c2a40c
Denis Wirries <denis@wirries.me>
2018-09-26 12:26:48 +0200

    use current dir for install

--- allocationsensor/install.sh
+++ allocationsensor/install.sh
@@ -3,6 +3,9 @@
 # This scripts install the systemd service.
 #
 
+APP_DIR=$(pwd -P)
+
 sudo cp src/allocationsensor.service  /etc/systemd/system/allocationsensor.service
+sudo sed -i -e "s~##APP_DIR##~$APP_DIR~g" /etc/systemd/system/allocationsensor.service
 sudo systemctl enable allocationsensor.service
 sudo systemctl start allocationsensor.service
