4a30f1c990776e3c2b2fa963ca1988eddc3d39f1
Denis Wirries <denis@wirries.me>
2018-09-26 12:46:01 +0200

    Read parameter from system environment

--- allocationsensor/start.sh
+++ allocationsensor/start.sh
@@ -7,4 +7,14 @@ SERVER=transportcontrol.local
 PORT=1111
 STATION=TCSAS001
 
+if [ "$TRANSPORTCONTROL_PORT" != "" ]; then
+    echo Setting port from environment
+    PORT=${TRANSPORTCONTROL_PORT}
+fi
+
+if [ "$TRANSPORTCONTROL_STATION" != "" ]; then
+    echo Setting station from environment
+    STATION=${TRANSPORTCONTROL_STATION}
+fi
+
 python src/allocationsensor.py ${SERVER} ${PORT} ${STATION}
