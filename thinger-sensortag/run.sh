1bd61e331f6ad600d207e5569c8afd5fc7a719f6
Denis Wirries <denis@wirries.me>
2018-09-30 16:24:26 +0200

    revert config

--- thinger-sensortag/run.sh
+++ thinger-sensortag/run.sh
@@ -8,8 +8,8 @@
 
 # Parameter for connection
 USERID=denisw
-DEVICEID=TEST
-DEVICECREDENTIAL=pasW0rdZ35D
+DEVICEID=gateway
+DEVICECREDENTIAL=password
 
 #SENSORTAGS="A0:E6:F8:AE:37:80=sensorYellow"
 SENSORTAGS="C4:BE:84:70:F6:8B=sensorRed A0:E6:F8:AE:37:80=sensorYellow B0:91:22:F6:CF:87=sensorViolet 54:6C:0E:4D:CC:02=sensorBlue 54:6C:0E:4D:5E:80=sensorOrange B0:91:22:F6:E6:02=sensorGreen"
